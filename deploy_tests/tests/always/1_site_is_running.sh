be451f9e14f1ca82b5b2718ca40bda6827283e22
HonzaMatosik <jan.matosik@gmail.com>
2016-08-18 09:39:21 +0200

    Better arguments handling

--- deploy_tests/tests/always/1_site_is_running.sh
+++ deploy_tests/tests/always/1_site_is_running.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+URL="$1"
+
+
 echo -n "Test site return code - "
 
-RETURN_CODE="$(curl "$1" --silent --show-error --head --location --output /dev/null --write-out '%{http_code}')"
+RETURN_CODE="$(curl "$URL" --silent --show-error --head --location --output /dev/null --write-out '%{http_code}')"
 if [[ "$RETURN_CODE" != "200" ]]; then
     echo "failed: $RETURN_CODE"
     exit 1
--- deploy_tests/tests/always/2_head_content_tests.sh
+++ deploy_tests/tests/always/2_head_content_tests.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+URL="$1"
+
 function check {
     if "$@"; then
         echo "OK"
@@ -15,7 +21,7 @@ function match {
 }
 
 echo "Test head content:"
-HEAD="$(curl --silent --show-error --location "$1" | tr -d "\n" | grep -o '<head>.*</head>')"
+HEAD="$(curl --silent --show-error --location "$URL" | tr -d "\n" | grep -o '<head>.*</head>')"
 
 
 echo -n "og:title - "
