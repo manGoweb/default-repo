3e3a2f428c6f472e74385a4d10247101d153355f
HonzaMatosik <jan.matosik@gmail.com>
2016-08-18 09:27:59 +0200

    Removed unnecessary escaping inside brace expansion

--- deploy_tests/start.sh
+++ deploy_tests/start.sh
@@ -7,7 +7,7 @@ STAGE=prod
 URL='mangoweb.cz'
 
 
-for TEST_CASE in deploy_tests/tests/{"$STAGE",always}/*.sh
+for TEST_CASE in deploy_tests/tests/{$STAGE,always}/*.sh
 do
 	/usr/bin/env bash "$TEST_CASE" "$URL"
 	echo ""
